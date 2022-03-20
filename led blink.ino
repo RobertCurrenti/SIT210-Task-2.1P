b9984c311bbeb014d60444ef83ce114c72362fdb
Robert Currenti <82944578+RobertCurrenti@users.noreply.github.com>
2022-03-21 10:50:33 +1100

    Update led blink.ino

--- led blink.ino
+++ led blink.ino
@@ -1,28 +1,17 @@
 // We define MY_LED to be the LED that we want to blink. 
-//
-// In this tutorial, we're using the blue D7 LED (next to D7 on the Photon 
-// and Electron, and next to the USB connector on the Argon and Boron).
 const pin_t MY_LED = D7;
 
 // measured in milliseconds
 const int unit_time = 150;
 
-// The following line is optional, but recommended in most firmware. It 
-// allows your code to run before the cloud is connected. In this case, 
-// it will begin blinking almost immediately instead of waiting until 
-// breathing cyan,
 SYSTEM_THREAD(ENABLED);
 
 // The setup() method is called once when the device boots.
 void setup() {
-	// In order to set a pin, you must tell Device OS that the pin is 
-	// an OUTPUT pin. This is often done from setup() since you only need 
-	// to do it once.
 	pinMode(MY_LED, OUTPUT);
 }
 
-
-
+// Makes the LED blink for a short time
 void dit(){
     digitalWrite(MY_LED, HIGH);
     delay(unit_time);
@@ -30,6 +19,7 @@ void dit(){
     delay(unit_time);
 }
 
+// Makes the LEd blink for a longer time
 void dah(){
     digitalWrite(MY_LED, HIGH);
     delay(unit_time*3);
@@ -37,11 +27,12 @@ void dah(){
     delay(unit_time);
 }
 
+// The length of time to wait between words in morse code
 void word_break(){
     delay(unit_time*4);
 }
 
-
+// Thes sequencse of dits and dahs for all the letters in  my name
 void B(){
     dah();
     dit();
@@ -69,8 +60,6 @@ void T(){
     dah();
     delay(unit_time*2);
 }
-
-
 
 // The loop() method is called frequently.
 void loop() {
